b0739badd5fcc86779215404dd4287f6261e6013
Vladimir Korkin <korkin.dev@gmail.com>
2023-02-18 21:41:51 +0100

    add function generateRandomColor to app.js

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const cols = document.querySelectorAll('.col');
 
+function generateRandomColor() {
+	// RGB 
+	// #FF0000 
+	const hexCodes = '0123456789ABCDEF';
+	let color = '';
+
+	for (let i = 0; i < 6; i++) {
+		color += hexCodes[Math.floor( Math.random() * hexCodes.length )];
+	}
+
+	return '#' + color;
+}
+
 function setRandomColors() {
 	cols.forEach((col) => {
 		console.log(col);
