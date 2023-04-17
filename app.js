5bacb25032ceb2b201903522e9fd2fece5805852
Vladimir Korkin <korkin.dev@gmail.com>
2023-04-17 07:02:16 +0200

    add function setTextColor to change text color of the color naming

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const cols = document.querySelectorAll('.col');
+const cols = document.querySelectorAll(".col");
 
 function generateRandomColor() {
-	// RGB 
-	// #FF0000 
-	const hexCodes = '0123456789ABCDEF';
-	let color = '';
+  // RGB
+  // #FF0000
+  const hexCodes = "0123456789ABCDEF";
+  let color = "";
 
-	for (let i = 0; i < 6; i++) {
-		color += hexCodes[Math.floor( Math.random() * hexCodes.length )];
-	}
+  for (let i = 0; i < 6; i++) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  }
 
-	return '#' + color;
+  return "#" + color;
 }
 
 function setRandomColors() {
-	cols.forEach((col) => {
-		const text = col.querySelector('h2');
-		const color = generateRandomColor();
+  cols.forEach((col) => {
+    const text = col.querySelector("h2");
+    const color = generateRandomColor();
+    // const color = chroma.random();
+    text.textContent = color;
+    col.style.background = color;
 
-		text.textContent = color;
-		col.style.background = color;
+    setTextColor(text, color);
+  });
+}
 
-	})
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > 0.5 ? "black" : "white";
 }
 
 setRandomColors();
